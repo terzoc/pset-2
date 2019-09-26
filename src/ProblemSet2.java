3b921ffb43e3a395715ec3b9a1a44eb2484b3179
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2019-09-26 09:18:02 -0400

    Exercise 4 works

--- src/ProblemSet2.java
+++ src/ProblemSet2.java
@@ -50,12 +50,12 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 1.00], print the number of dollar
          * bills, quarters, dimes, nickels, and pennies needed to produce this amount.
          */
-         final double DOLLAR_VALUE = 100;
-         final double QUARTER_VALUE = 25;
-         final double DIME_VALUE = 10;
-         final double NICKEL_VALUE = 5;
-         final double PENNIE_VALUE = 1;
-         double leftOver;
+         // final double DOLLAR_VALUE = 100;
+         // final double QUARTER_VALUE = 25;
+         // final double DIME_VALUE = 10;
+         // final double NICKEL_VALUE = 5;
+         // final double PENNIE_VALUE = 1;
+         // double leftOver;
 
          // System.out.print("\nEnter dollar amount: ");
          // leftOver = in.nextDouble();
@@ -81,30 +81,30 @@ public class ProblemSet2 {
          * Given a dollar amount in the range [0.00, 20.00], print the smallest number of
          * bills and coins needed to produce this amount.
          */
-         final double FIVE_BILL = 500;
-         final double TEN_BILL = 1000;
-         final double TWENTY_BILL = 2000;
-
-         System.out.print("\nEnter dollar amount: ");
-         leftOver = in.nextDouble();
-         leftOver *= 100;
-         int bills = (int)(leftOver/TWENTY_BILL);
-         leftOver %= TWENTY_BILL;
-         bills += (int)(leftOver / TEN_BILL);
-         leftOver %= TEN_BILL;
-         bills += (int)(leftOver / FIVE_BILL);
-         leftOver %= FIVE_BILL;
-         bills += (int)(leftOver / DOLLAR_VALUE);
-         leftOver %= DOLLAR_VALUE;
-         int coins =(int)(leftOver/QUARTER_VALUE);
-         leftOver %= QUARTER_VALUE;
-         coins +=(int)(leftOver/DIME_VALUE);
-         leftOver %= DIME_VALUE;
-         coins +=(int)(leftOver/NICKEL_VALUE);
-         leftOver %= NICKEL_VALUE;
-         coins +=(int) (leftOver/PENNIE_VALUE);
-         System.out.println("\nBILLS\t: " + bills);
-         System.out.println("COINS\t: " + coins);
+         // final double FIVE_BILL = 500;
+         // final double TEN_BILL = 1000;
+         // final double TWENTY_BILL = 2000;
+         //
+         // System.out.print("\nEnter dollar amount: ");
+         // leftOver = in.nextDouble();
+         // leftOver *= 100;
+         // int bills = (int)(leftOver/TWENTY_BILL);
+         // leftOver %= TWENTY_BILL;
+         // bills += (int)(leftOver / TEN_BILL);
+         // leftOver %= TEN_BILL;
+         // bills += (int)(leftOver / FIVE_BILL);
+         // leftOver %= FIVE_BILL;
+         // bills += (int)(leftOver / DOLLAR_VALUE);
+         // leftOver %= DOLLAR_VALUE;
+         // int coins =(int)(leftOver/QUARTER_VALUE);
+         // leftOver %= QUARTER_VALUE;
+         // coins +=(int)(leftOver/DIME_VALUE);
+         // leftOver %= DIME_VALUE;
+         // coins +=(int)(leftOver/NICKEL_VALUE);
+         // leftOver %= NICKEL_VALUE;
+         // coins +=(int) (leftOver/PENNIE_VALUE);
+         // System.out.println("\nBILLS\t: " + bills);
+         // System.out.println("COINS\t: " + coins);
 
 
         /*
@@ -113,6 +113,22 @@ public class ProblemSet2 {
          * Given a number of inches, print the equivalent number of miles, yards, feet,
          * and inches.
          */
+         final double INCH_IN_MILE = 63360;
+         final double INCH_IN_YARD = 36;
+         final double INCH_IN_FOOT = 12;
+
+         System.out.print("Enter a number on inches: ");
+         double inchesRemaining = in.nextDouble();
+         int miles = (int) (inchesRemaining / INCH_IN_MILE);
+         inchesRemaining %= INCH_IN_MILE;
+         int yards = (int) (inchesRemaining / INCH_IN_YARD);
+         inchesRemaining %= INCH_IN_YARD;
+         int feet = (int) (inchesRemaining / INCH_IN_FOOT);
+         int finalInches = (int) (inchesRemaining % INCH_IN_FOOT);
+         System.out.println("\nMILES\t: " + miles);
+         System.out.println("YARDS\t: " + yards);
+         System.out.println("FEET\t: " + feet);
+         System.out.println("Inches\t: " + finalInches);
 
 
 
